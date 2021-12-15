b22afccbc38a67c134e282fa9af384fd1c891cb3
Dung <dddung487@gmail.com>
2021-12-15 23:48:08 +0700

    Complete The Game
    Not Fixing the code for change the score

---
+++ Assets/Script/ScoreText.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class ScoreText : MonoBehaviour
+{
+    // private int score;
+    //
+    // private TextMeshProGUI text;
+    // // Start is called before the first frame update
+    // private void Start()
+    // {
+    //     text = GetComponent<TMPro.TextMeshProGUI>();
+    //     GameManager.OnCubeSpawned += GameManager_OnCubeSpawned;
+    // }
+    //
+    // private void OnDestroy()
+    // {
+    //     GameManager.OnCubeSpawned -= GameManager_OnCubeSpawned;
+    // }
+    //
+    // private void GameManager_OnCubeSpawned()
+    // {
+    //     score++;
+    //     text.text = "Score: " + score;
+    // }
+
+}
